1a5729ba4976f1a319946641c28ae53ad600f861
max <skrimerw@gmail.com>
2025-09-16 11:12:55 +0300

    fix

--- lib/api-client/songs.ts
+++ lib/api-client/songs.ts
@@ -1,14 +1,13 @@
-import { Song } from "@prisma/client";
 import { axiosInstance } from "./axiosInstance";
 import { SongWithAlbumAndArtist } from "@/types";
 
 export const search = async (searchVal: string) => {
-    return await axiosInstance.get<SongWithAlbumAndArtist[]>(
-        "/api/songs/search",
-        {
-            params: {
-                search: searchVal,
-            },
-        }
-    );
+  return await axiosInstance.get<SongWithAlbumAndArtist[]>(
+    "/api/songs/search",
+    {
+      params: {
+        search: searchVal,
+      },
+    }
+  );
 };
--- store/use-audio-player.ts
+++ store/use-audio-player.ts
@@ -4,136 +4,143 @@ import { SongWithAlbumAndArtist } from "@/types";
 import { create } from "zustand";
 
 type AudioPlayerState = {
-    currentSong: SongWithAlbumAndArtist | null;
-    queue: SongWithAlbumAndArtist[];
-    isPaused: boolean;
-    isEnded: boolean;
-    volume: number;
-    currentTime: number;
-    duration: number;
+  queueId: string | null;
+  currentSong: SongWithAlbumAndArtist | null;
+  queue: SongWithAlbumAndArtist[];
+  isPaused: boolean;
+  isEnded: boolean;
+  volume: number;
+  currentTime: number;
+  duration: number;
 
-    play: (song?: SongWithAlbumAndArtist) => void;
-    pause: () => void;
-    next: () => void;
-    prev: () => void;
-    setVolume: (val: number) => void;
-    setQueue: (songs: SongWithAlbumAndArtist[]) => void;
-    seekTo: (val: number) => void;
+  play: (song?: SongWithAlbumAndArtist) => void;
+  pause: () => void;
+  next: () => void;
+  prev: () => void;
+  setVolume: (val: number) => void;
+  setQueue: (songs: SongWithAlbumAndArtist[]) => void;
+  seekTo: (val: number) => void;
+  setQueueId: (val: string) => void;
 };
 
 export const useAudioPlayer = create<AudioPlayerState>((set, get) => {
-    const audio = typeof Audio !== "undefined" ? new Audio() : null;
+  const audio = typeof Audio !== "undefined" ? new Audio() : null;
 
-    if (audio) {
-        audio.volume = 0.5;
+  if (audio) {
+    audio.volume = 0.5;
 
-        audio.addEventListener("timeupdate", () => {
-            set({
-                currentTime: audio.currentTime,
-                duration: audio.duration || 0,
-            });
+    audio.addEventListener("timeupdate", () => {
+      set({
+        currentTime: audio.currentTime,
+        duration: audio.duration || 0,
+      });
+    });
+
+    audio.addEventListener("ended", () => {
+      set({ isEnded: true, isPaused: true });
+      get().next();
+    });
+
+    audio.addEventListener("play", () => {
+      set({ isPaused: false, isEnded: false });
+    });
+
+    audio.addEventListener("pause", () => {
+      set({ isPaused: true });
+    });
+  }
+
+  return {
+    currentSong: null,
+    queue: [],
+    isPaused: true,
+    isEnded: false,
+    volume: 0.5,
+    currentTime: 0,
+    duration: 0,
+    queueId: null,
+
+    play: (song?: SongWithAlbumAndArtist) => {
+      if (!audio) return;
+
+      const { currentSong, queue } = get();
+      const songForMetadata = song || currentSong;
+
+      if (song) {
+        set({ currentSong: song, isPaused: false, isEnded: false });
+
+        if (!queue.find((s) => s.id === song.id)) {
+          set({ queue: [...queue, song] });
+        }
+
+        audio.src = song.audioUrl;
+      }
+
+      if ("mediaSession" in navigator && songForMetadata) {
+        navigator.mediaSession.metadata = new MediaMetadata({
+          title: songForMetadata.title,
+          artist: songForMetadata.artist.name,
+          artwork: [
+            {
+              src: songForMetadata.album.imageUrl || "",
+              sizes: "512x512",
+              type: "image/jpeg",
+            },
+          ],
         });
+      }
 
-        audio.addEventListener("ended", () => {
-            set({ isEnded: true, isPaused: true });
-            get().next();
-        });
+      audio.play();
+    },
 
-        audio.addEventListener("play", () => {
-            set({ isPaused: false, isEnded: false });
-        });
+    pause: () => {
+      if (!audio) return;
+      audio.pause();
+    },
 
-        audio.addEventListener("pause", () => {
-            set({ isPaused: true });
-        });
-    }
+    next: () => {
+      if (!audio) return;
 
-    return {
-        currentSong: null,
-        queue: [],
-        isPaused: false,
-        isEnded: false,
-        volume: 0.5,
-        currentTime: 0,
-        duration: 0,
+      const { queue, currentSong } = get();
+      if (queue.length === 0 || !currentSong) return;
 
-        play: (song?: SongWithAlbumAndArtist) => {
-            if (!audio) return;
+      let newIndex = queue.findIndex((s) => s.id === currentSong.id) + 1;
+      if (newIndex === queue.length) newIndex = 0;
 
-            const { currentSong, queue } = get();
-            const songForMetadata = song || currentSong;
+      get().play(queue[newIndex]);
+    },
 
-            if (song) {
-                set({ currentSong: song, isPaused: false, isEnded: false });
+    prev: () => {
+      if (!audio) return;
 
-                if (!queue.find((s) => s.id === song.id)) {
-                    set({ queue: [...queue, song] });
-                }
+      const { queue, currentSong } = get();
+      if (queue.length === 0 || !currentSong) return;
 
-                audio.src = song.audioUrl;
-            }
+      let newIndex = queue.findIndex((s) => s.id === currentSong.id) - 1;
+      if (newIndex < 0) newIndex = queue.length - 1;
 
-            if ("mediaSession" in navigator && songForMetadata) {
-                navigator.mediaSession.metadata = new MediaMetadata({
-                    title: songForMetadata.title,
-                    artist: songForMetadata.artist.name,
-                    artwork: [
-                        {
-                            src: songForMetadata.album.imageUrl || "",
-                            sizes: "512x512",
-                            type: "image/jpeg",
-                        },
-                    ],
-                });
-            }
+      get().play(queue[newIndex]);
+    },
 
-            audio.play();
-        },
+    setVolume: (val: number) => {
+      if (!audio) return;
+      audio.volume = val;
+      set({ volume: val });
+    },
 
-        pause: () => {
-            if (!audio) return;
-            audio.pause();
-        },
+    setQueue: (songs: SongWithAlbumAndArtist[]) => {
+      set({ queue: songs });
+    },
 
-        next: () => {
-            if (!audio) return;
+    seekTo: (val: number) => {
+      if (!audio) return;
+      if (val > audio.duration) val = audio.duration;
+      audio.currentTime = val;
+      set({ currentTime: val });
+    },
 
-            const { queue, currentSong } = get();
-            if (queue.length === 0 || !currentSong) return;
-
-            let newIndex = queue.findIndex((s) => s.id === currentSong.id) + 1;
-            if (newIndex === queue.length) newIndex = 0;
-
-            get().play(queue[newIndex]);
-        },
-
-        prev: () => {
-            if (!audio) return;
-
-            const { queue, currentSong } = get();
-            if (queue.length === 0 || !currentSong) return;
-
-            let newIndex = queue.findIndex((s) => s.id === currentSong.id) - 1;
-            if (newIndex < 0) newIndex = queue.length - 1;
-
-            get().play(queue[newIndex]);
-        },
-
-        setVolume: (val: number) => {
-            if (!audio) return;
-            audio.volume = val;
-            set({ volume: val });
-        },
-
-        setQueue: (songs: SongWithAlbumAndArtist[]) => {
-            set({ queue: songs });
-        },
-
-        seekTo: (val: number) => {
-            if (!audio) return;
-            if (val > audio.duration) val = audio.duration;
-            audio.currentTime = val;
-            set({ currentTime: val });
-        },
-    };
+    setQueueId: (val) => {
+      set({ queueId: val });
+    },
+  };
 });
